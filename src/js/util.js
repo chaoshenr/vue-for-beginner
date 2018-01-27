5b2dbf61c105ccf6b200b0198133fde81c706f98
lijinchao <jinchao233@gmail.com>
2018-01-27 14:05:15 +0800

    util

--- src/js/util.js
+++ src/js/util.js
@@ -15,3 +15,29 @@ export function dateFormat(fmt, date) { //author: meizz
             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
+
+export function getScrollTop() {
+    if( document.documentElement && document.documentElement.scollTop ) {
+        return document.documentElement.scrollTop;
+    } else if ( document.body ) {
+        return document.body.scrollTop
+    }
+    return new Error("environment")
+}
+
+export function getClientHeight() {
+    if( document.body.clientHeight && document.documentElement.clientHeight ) {
+        return Math.min(document.body.clientHeight, document.documentElement.clientHeight);        
+    } else {
+        return Math.max(document.body.clientHeight, document.documentElement.clientHeight);   
+    }
+}
+export function getScrollHeight() {
+    return Math.max(document.body.scrollHeight,document.documentElement.scrollHeight);
+}
+export default {
+    dateFormat: dateFormat,
+    getScrollTop: getScrollTop,
+    getClientHeight: getClientHeight,
+    getScrollHeight: getScrollHeight
+}
